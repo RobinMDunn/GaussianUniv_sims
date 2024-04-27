42cecf89ee7691b954960780d1e0d5a13e540ad0
RobinMDunn <robin.dunn1012@gmail.com>
2024-04-26 22:13:04 -0400

    Modify data locations for Figures 1 and 2

--- sim_plot_code/figure_01.R
+++ sim_plot_code/figure_01.R
@@ -1,4 +1,4 @@
-# Plots showing regions of LRT coverage
+# Plot showing regions of LRT coverage
 
 library(tidyverse)
 library(data.table)
@@ -19,7 +19,7 @@ paper_theme <- theme_bw() +
 
 # Read in data
 grid_data_usual <- 
-  fread(file = "data/region/fixed_theta_fixed_data/00_normal_usual_LRT.csv") %>% 
+  fread(file = "sim_data/fig01_classical_LRT.csv") %>% 
   mutate(sim = factor(sim, levels = 1:6,
                       labels = c("Sim 1", "Sim 2", "Sim 3",
                                  "Sim 4", "Sim 5", "Sim 6")),
@@ -28,7 +28,7 @@ grid_data_usual <-
   as.data.table()
 
 grid_data_split <- 
-  fread(file = "data/region/fixed_theta_fixed_data/01_normal_split_LRT.csv") %>% 
+  fread(file = "sim_data/fig01_split_LRT.csv") %>% 
   mutate(sim = factor(sim, levels = 1:6,
                       labels = c("Sim 1", "Sim 2", "Sim 3",
                                  "Sim 4", "Sim 5", "Sim 6"))) %>%
@@ -38,7 +38,7 @@ grid_data_split <-
   as.data.table()
 
 grid_data_crossfit <- 
-  fread(file = "data/region/fixed_theta_fixed_data/02_normal_crossfit_LRT.csv") %>% 
+  fread(file = "sim_data/fig01_crossfit_LRT.csv") %>% 
   mutate(sim = factor(sim, levels = 1:6,
                       labels = c("Sim 1", "Sim 2", "Sim 3",
                                  "Sim 4", "Sim 5", "Sim 6"))) %>%
@@ -48,7 +48,7 @@ grid_data_crossfit <-
   as.data.table()
 
 grid_data_subsample <- 
-  fread(file = "data/region/fixed_theta_fixed_data/03_normal_subsample.csv") %>% 
+  fread(file = "sim_data/fig01_subsample_LRT.csv") %>% 
   mutate(sim = factor(sim, levels = 1:6,
                       labels = c("Sim 1", "Sim 2", "Sim 3",
                                  "Sim 4", "Sim 5", "Sim 6")),
@@ -84,57 +84,10 @@ grid_plot <- grid_data %>%
         aspect.ratio = 1,
         axis.text = element_text(size = 9))
 
-grid_plot_linetype <- grid_data %>%
-  ggplot(aes(x = coord1, y = coord2, col = Method, linetype = Method)) +
-  geom_convexhull(alpha = 0, aes(fill = Method, linetype = Method)) +
-  facet_wrap(. ~ sim) +
-  labs(x = "Coordinate 1",
-       y = "Coordinate 2",
-       title = "Coverage Regions of Multivariate Normal LRT Methods at d = 2",
-       subtitle = expression("Fixed"~paste(theta, "*")~"= (0, 0),"~
-                               "Fixed Data (n = 1000), Random Subsampling")) +
-  scale_color_manual(values = c("black", "blue", "red", "orange")) +
-  scale_linetype_manual(values = c("twodash", "dotdash", "longdash", "solid")) +
-  paper_theme +
-  theme(legend.text = element_text(margin = margin(t = 2, b = 10, unit = "pt")),
-        legend.key.size = unit(1, "cm"),
-        aspect.ratio = 1,
-        axis.text = element_text(size = 9),
-        legend.key.width = unit(1.2, "cm")) 
-
-two_universal_plots <- grid_data %>%
-  dplyr::filter(sim %in% c("Sim 1", "Sim 2"),
-                Method != "Classical LRT") %>% 
-  ggplot(aes(x = coord1, y = coord2, col = Method)) +
-  geom_convexhull(alpha = 0, aes(fill = Method)) +
-  facet_wrap(. ~ sim) +
-  labs(x = "Coordinate 1",
-       y = "Coordinate 2",
-       title = "Gaussian universal LRT confidence sets") +
-  scale_color_manual(values = c("blue", "red", "orange")) +
-  paper_theme +
-  theme(legend.text = element_text(margin = margin(t = 2, b = 10, unit = "pt")),
-        legend.key.size = unit(1, "cm"),
-        aspect.ratio = 1,
-        axis.text = element_text(size = 9),
-        legend.key.width = unit(1.2, "cm")) 
-
 ######################
 ##### Save plots #####
 ######################
 
-ggsave(plot = grid_plot,
-       filename = "plots/region/fixed_theta_fixed_data/04_fixed_data_LRT_grid.pdf",
-       width = 8.5, height = 5)
-
 ggsave(plot = grid_plot + labs(title = NULL, subtitle = NULL),
-       filename = "plots/region/fixed_theta_fixed_data/04_fixed_data_LRT_grid_journal.pdf",
+       filename = "sim_plots/figure_01.pdf",
        width = 8.5, height = 4.5)
-
-ggsave(plot = grid_plot_linetype + labs(title = NULL, subtitle = NULL),
-       filename = "plots/region/fixed_theta_fixed_data/04_fixed_data_LRT_grid_linetype_journal.pdf",
-       width = 8.5, height = 4.5)
-
-ggsave(plot = two_universal_plots,
-       filename = "plots/region/fixed_theta_fixed_data/04_two_universal_plots.pdf",
-       width = 6.5, height = 3)
--- sim_plot_code/figure_02.R
+++ sim_plot_code/figure_02.R
@@ -18,7 +18,7 @@ paper_theme <- theme_bw() +
         panel.spacing = unit(1.2, "lines"))
 
 # Read in data
-results <- fread("data/radius/08_ddim_limit_results.csv")
+results <- fread("sim_data/fig02.csv")
 
 # Plot of simulated expectation and analytical expression values
 expectation_plot <- results %>% 
@@ -50,49 +50,10 @@ expectation_plot <- results %>%
   paper_theme +
   theme(axis.text = element_text(size = 10))
 
-# Plot of simulated expectation and analytical expression values,
-# with single line and single point for legend
-expectation_plot_new_legend <- results %>% 
-  filter(d %in% c(1, 20), n %in% c(10, 50, 200)) %>% 
-  mutate(d_and_n = factor(paste0("d = ", d, ", n = ", n),
-                          levels = c("d = 1, n = 10", "d = 1, n = 50",   
-                                     "d = 1, n = 200", "d = 20, n = 10",
-                                     "d = 20, n = 50", "d = 20, n = 200"),
-                          labels = c("d = 1, n = 10", "d = 1, n = 50",   
-                                     "d = 1, n = 200", "d = 20, n = 10",
-                                     "d = 20, n = 50", "d = 20, n = 200"))) %>% 
-  ggplot(aes(x = c_val)) +
-  facet_wrap(~ d_and_n, scales = "free") +
-  geom_line(data = . %>% filter(method == "Analytical\nexpression"),
-            aes(y = value, linetype = "Analytical\nexpression\n"),
-            alpha = 0.7, na.rm = T, col = "red") +
-  geom_point(data = . %>% filter(method == "Simulated\nexpectation"),
-             aes(y = value, shape = "Simulated\nexpectation\n"),
-             alpha = 0.7, col = "black") +
-  geom_hline(yintercept = 10, lty = "dashed", col = "black") +
-  labs(title = expression(atop("Simulated Expectation vs Analytical Expression", 
-                               "for Limiting Subsampling Test Statistic as"~
-                                 B~symbol('\256')~infinity)),
-       x = TeX("$c$, where $\\theta = c\\textbf{1}$"),
-       y = "Value",
-       col = "Method") +
-  paper_theme +
-  scale_linetype_discrete(name = NULL) +
-  scale_shape_discrete(name = NULL) +
-  theme(axis.text = element_text(size = 10))
-
 ######################
 ##### Save plots #####
 ######################
 
-ggsave(plot = expectation_plot,
-       filename = "plots/radius/08_expect_analytical_panel.pdf",
-       width = 9, height = 5)
-
 ggsave(plot = expectation_plot + labs(title = NULL, subtitle = NULL),
-       filename = "plots/radius/08_expect_analytical_panel_journal.pdf",
-       width = 9, height = 4)
-
-ggsave(plot = expectation_plot_new_legend + labs(title = NULL, subtitle = NULL),
-       filename = "plots/radius/08_expect_analytical_new_legend_journal.pdf",
+       filename = "sim_plots/figure_02.pdf",
        width = 9, height = 4)
